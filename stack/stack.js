e0f49a4e4f15e2fdc875b11b5cfee927a86e41b1
veshant <veshantdahiya@gmail.com>
2024-08-09 21:06:29 +0530

    add some array methods on stack

--- stack/stack.js
+++ stack/stack.js
@@ -18,6 +18,17 @@ class myStack{
     size(){
         return this.array.length;
     }
+    reverse(){
+        return this.array.reverse();
+    }
+    printElements(){
+        let str = "Elements in array are :- "
+        console.log(this.array, "print array");
+        for (let i = 0; i < this.array.length; i++) {
+            str += this.array[i] + " | ";
+        }
+        return str;
+    }
 }
 
 const stack = new myStack();
@@ -25,9 +36,10 @@ stack.push(1);
 stack.push(2);
 stack.push(3);
 stack.push(4);
+console.log(stack.reverse());
 console.log(stack.peek());
 console.log(stack.pop());
 console.log(stack.peek());
 console.log(stack.isEmpty());
 console.log(stack.size());
-console.log(stack.array);
+console.log(stack.printElements());
