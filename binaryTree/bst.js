32c31ee41b7cc4c5e6efc818e628d48c157843c4
veshant <veshantdahiya@gmail.com>
2024-09-12 14:18:00 +0530

    add bfs

--- binaryTree/bst.js
+++ binaryTree/bst.js
@@ -115,4 +115,24 @@ class bst{
         return values;
     }
 
+    bfs(root){
+        if (root == null) {
+            return;
+        }
+        const values = [];
+        const queue = [];
+
+        while (queue.length > 0) {
+            const node = queue.shift();
+            values.push(node.key);
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+            if (node.right !== null) {
+                queue.push(node.right);
+            }
+            
+        }        
+        return values;
+    }
 }
