69eb6ddfbdea51d4c3924d844cc2672ffbed35ad
veshant <veshantdahiya@gmail.com>
2024-09-15 16:44:19 +0530

    add first occurance

---
+++ recursion/findFirstOccurance.js
@@ -0,0 +1,30 @@
+// find the first occurance of the given element
+
+const arr = [3,2,1,4,6,5,1]
+const key = 1
+
+function firstOccur(ary) {   
+    let foundAt = []
+    for (let i = 0; i < ary.length; i++) {
+        console.log(i);
+        if (key==ary[i]) {
+         foundAt.push(i); 
+        }
+    }
+    return foundAt;
+}
+// console.log(`element found at position = ${firstOccur(arr)}`);
+
+function firstOccurR(ary, i = 0, foundAt=[]) {
+    if (i == ary.length){
+        if (foundAt.length ==0) {
+            return -1;
+        }
+        return foundAt;
+    }
+    if (ary[i]==key) {
+        foundAt.push(i)
+    }
+    return firstOccurR(ary, i+1, foundAt)
+}
+console.log(firstOccurR(arr));
